d8f1ccf42e484f036a4174afc3e5fdce83edd7e1
AustinPuk <austinpuk@gmail.com>
2018-11-30 20:58:28 -0800

    Camera following player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    GameObject target;
+
+    [SerializeField]
+    Vector3 initialOffset = new Vector3(0.0f, 13.0f, -7.5f);
+
+    void Start()
+    {
+        if (!target)
+            throw new System.Exception("ERROR : Camera needs a target");
+
+        transform.position = target.transform.position + initialOffset;
+    }
+
+    void Update()
+    {
+        FollowPlayer();
+
+        // TODO : Zoom In, Zoom Out with Mouse Wheel, Bounds on edges of arena, lerped camera movement?
+    }
+
+    void FollowPlayer()
+    {
+        transform.position = target.transform.position + initialOffset;
+    }
+}
