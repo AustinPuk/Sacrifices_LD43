6ee05a2403675967747dc63040234f663d37e76a
AustinPuk <austinpuk@gmail.com>
2018-11-30 20:40:19 -0800

    Basic movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float movementSpeed = 5;
+
+    Vector3 movementVector;
+
+    void Start ()
+    {
+        // TODO : 
+    }
+
+    void Update ()
+    {
+        ProcessInput();
+        HandleMovement();
+    }
+
+    void ProcessInput()
+    {
+        // Gets Movement Input
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        movementVector = new Vector3(h, 0, v);
+    }
+
+    void HandleMovement()
+    {
+        // Character tries to face in direction of movement vector.
+        transform.Translate(movementVector * movementSpeed * Time.deltaTime, Space.World);
+        if (movementVector != Vector3.zero)
+            transform.LookAt(transform.position + Vector3.Slerp(transform.forward, movementVector, 0.8f), transform.up);
+    }
+}
